289f520c52ce87053f34d0dde400796e6d069da7
mirra-09 <balamirra.2023@vitstudent.ac.in>
2026-02-15 11:06:38 +0530

    Update 03_exploratory_analysis.R

--- scripts/03_exploratory_analysis.R
+++ scripts/03_exploratory_analysis.R
@@ -17,8 +17,15 @@ p <- ggplot(data.frame(
   PC2 = pca$x[,2],
   Type = sample_type
 ), aes(x = PC1, y = PC2, color = Type)) +
-  geom_point(size = 2) +
-  theme_minimal() +
-  ggtitle("PCA of TCGA BRCA Samples")
+  geom_point(size = 3, alpha = 0.8) +
+  theme_classic() +
+  labs(
+    title = "PCA of TCGA BRCA Samples",
+    x = "Principal Component 1",
+    y = "Principal Component 2"
+  ) +
+  scale_color_manual(values = c("TP" = "#E64B35", 
+                                "NT" = "#4DBBD5"))
 
 ggsave("results/pca_plot.png", plot = p, width = 8, height = 6)
+
